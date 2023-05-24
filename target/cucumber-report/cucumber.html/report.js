fc986ba416d7b04b3cb79bc96c46c29be15ffd63
Nita0310 <neetgabani@gmail.com>
2023-05-24 13:44:36 +0100

    orangehrm-cucumber-wk-16

---
+++ target/cucumber-report/cucumber.html/report.js
@@ -0,0 +1,258 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("logintest.feature");
+formatter.feature({
+  "line": 1,
+  "name": "As a user I want to verify different login functions.",
+  "description": "",
+  "id": "as-a-user-i-want-to-verify-different-login-functions.",
+  "keyword": "Feature"
+});
+formatter.before({
+  "duration": 6536786600,
+  "status": "passed"
+});
+formatter.background({
+  "line": 3,
+  "name": "",
+  "description": "",
+  "type": "background",
+  "keyword": "Background"
+});
+formatter.step({
+  "line": 4,
+  "name": "I am on HomePage \u0026 Login to the application",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "UserTestSteps.iAmOnHomePageLoginToTheApplication()"
+});
+formatter.result({
+  "duration": 1293353100,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 7,
+  "name": "I should login successfully",
+  "description": "",
+  "id": "as-a-user-i-want-to-verify-different-login-functions.;i-should-login-successfully",
+  "type": "scenario",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "line": 6,
+      "name": "@sanity"
+    },
+    {
+      "line": 6,
+      "name": "@regression"
+    }
+  ]
+});
+formatter.step({
+  "line": 8,
+  "name": "I see Welcome message",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "LoginTestSteps.iSeeWelcomeMessage()"
+});
+formatter.result({
+  "duration": 235509100,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 688655200,
+  "status": "passed"
+});
+formatter.uri("userstest.feature");
+formatter.feature({
+  "line": 1,
+  "name": "As a user I want to verify admin can execute different actions on users.",
+  "description": "",
+  "id": "as-a-user-i-want-to-verify-admin-can-execute-different-actions-on-users.",
+  "keyword": "Feature"
+});
+formatter.before({
+  "duration": 2423682900,
+  "status": "passed"
+});
+formatter.background({
+  "line": 3,
+  "name": "",
+  "description": "",
+  "type": "background",
+  "keyword": "Background"
+});
+formatter.step({
+  "line": 4,
+  "name": "I am on HomePage \u0026 Login to the application",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "UserTestSteps.iAmOnHomePageLoginToTheApplication()"
+});
+formatter.result({
+  "duration": 568609900,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 7,
+  "name": "Admin should be able to add users",
+  "description": "",
+  "id": "as-a-user-i-want-to-verify-admin-can-execute-different-actions-on-users.;admin-should-be-able-to-add-users",
+  "type": "scenario",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "line": 6,
+      "name": "@sanity"
+    },
+    {
+      "line": 6,
+      "name": "@regression"
+    }
+  ]
+});
+formatter.step({
+  "line": 8,
+  "name": "I click On Admin Tab",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 9,
+  "name": "I Verify System Users Text",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 10,
+  "name": "I click On Add button",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 11,
+  "name": "I Verify Add User Text",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 12,
+  "name": "I Select User Role Admin",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 13,
+  "name": "I enter Employee Name Ananya Dash",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 14,
+  "name": "I enter User name",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 15,
+  "name": "I Select status Disable",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 16,
+  "name": "I enter password",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 17,
+  "name": "I enter Confirm Password",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 18,
+  "name": "I click On Save Button",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 19,
+  "name": "I verify message Successfully Saved",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "UserTestSteps.iClickOnAdminTab()"
+});
+formatter.result({
+  "duration": 3370666300,
+  "status": "passed"
+});
+formatter.match({
+  "location": "UserTestSteps.iVerifySystemUsersText()"
+});
+formatter.result({
+  "duration": 202949000,
+  "status": "passed"
+});
+formatter.match({
+  "location": "UserTestSteps.iClickOnAddButton()"
+});
+formatter.result({
+  "duration": 399296000,
+  "status": "passed"
+});
+formatter.match({
+  "location": "UserTestSteps.iVerifyAddUserText()"
+});
+formatter.result({
+  "duration": 168986400,
+  "status": "passed"
+});
+formatter.match({
+  "location": "UserTestSteps.iSelectUserRoleAdmin()"
+});
+formatter.result({
+  "duration": 32667827300,
+  "status": "passed"
+});
+formatter.match({
+  "location": "UserTestSteps.iEnterEmployeeNameAnanyaDash()"
+});
+formatter.result({
+  "duration": 61652400300,
+  "error_message": "org.openqa.selenium.NoSuchElementException: no such element: Unable to locate element: {\"method\":\"xpath\",\"selector\":\"//*[@class\u003d\u0027oxd-autocomplete-wrapper\u0027]//*[contains(text(),\u0027Goutam  Ganesh\u0027)]\"}\n  (Session info: chrome\u003d113.0.5672.127)\nFor documentation on this error, please visit: https://selenium.dev/exceptions/#no_such_element\nBuild info: version: \u00274.9.0\u0027, revision: \u0027d7057100a6\u0027\nSystem info: os.name: \u0027Windows 10\u0027, os.arch: \u0027amd64\u0027, os.version: \u002710.0\u0027, java.version: \u00271.8.0_121\u0027\nDriver info: org.openqa.selenium.chrome.ChromeDriver\nCommand: [db9f0f17ccc8c9029d564e121848e9d1, findElement {using\u003dxpath, value\u003d//*[@class\u003d\u0027oxd-autocomplete-wrapper\u0027]//*[contains(text(),\u0027Goutam  Ganesh\u0027)]}]\nCapabilities {acceptInsecureCerts: false, browserName: chrome, browserVersion: 113.0.5672.127, chrome: {chromedriverVersion: 113.0.5672.63 (0e1a4471d5ae..., userDataDir: C:\\Users\\neetg\\AppData\\Loca...}, goog:chromeOptions: {debuggerAddress: localhost:56052}, networkConnectionEnabled: false, pageLoadStrategy: normal, platformName: WINDOWS, proxy: Proxy(), se:cdp: ws://localhost:56052/devtoo..., se:cdpVersion: 113.0.5672.127, setWindowRect: true, strictFileInteractability: false, timeouts: {implicit: 0, pageLoad: 300000, script: 30000}, unhandledPromptBehavior: dismiss and notify, webauthn:extension:credBlob: true, webauthn:extension:largeBlob: true, webauthn:extension:minPinLength: true, webauthn:extension:prf: true, webauthn:virtualAuthenticators: true}\nSession ID: db9f0f17ccc8c9029d564e121848e9d1\r\n\tat sun.reflect.NativeConstructorAccessorImpl.newInstance0(Native Method)\r\n\tat sun.reflect.NativeConstructorAccessorImpl.newInstance(NativeConstructorAccessorImpl.java:62)\r\n\tat sun.reflect.DelegatingConstructorAccessorImpl.newInstance(DelegatingConstructorAccessorImpl.java:45)\r\n\tat java.lang.reflect.Constructor.newInstance(Constructor.java:423)\r\n\tat org.openqa.selenium.remote.codec.w3c.W3CHttpResponseCodec.createException(W3CHttpResponseCodec.java:200)\r\n\tat org.openqa.selenium.remote.codec.w3c.W3CHttpResponseCodec.decode(W3CHttpResponseCodec.java:133)\r\n\tat org.openqa.selenium.remote.codec.w3c.W3CHttpResponseCodec.decode(W3CHttpResponseCodec.java:53)\r\n\tat org.openqa.selenium.remote.HttpCommandExecutor.execute(HttpCommandExecutor.java:193)\r\n\tat org.openqa.selenium.remote.service.DriverCommandExecutor.invokeExecute(DriverCommandExecutor.java:183)\r\n\tat org.openqa.selenium.remote.service.DriverCommandExecutor.execute(DriverCommandExecutor.java:158)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.execute(RemoteWebDriver.java:543)\r\n\tat org.openqa.selenium.remote.ElementLocation$ElementFinder$2.findElement(ElementLocation.java:162)\r\n\tat org.openqa.selenium.remote.ElementLocation.findElement(ElementLocation.java:60)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElement(RemoteWebDriver.java:352)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElement(RemoteWebDriver.java:344)\r\n\tat org.openqa.selenium.support.pagefactory.DefaultElementLocator.findElement(DefaultElementLocator.java:70)\r\n\tat org.openqa.selenium.support.pagefactory.internal.LocatingElementHandler.invoke(LocatingElementHandler.java:39)\r\n\tat com.sun.proxy.$Proxy23.click(Unknown Source)\r\n\tat com.orangehrm.utility.Utility.clickOnElement(Utility.java:51)\r\n\tat com.orangehrm.pages.ViewSystemUsersPage.enterEmp(ViewSystemUsersPage.java:97)\r\n\tat com.orangehrm.steps.UserTestSteps.iEnterEmployeeNameAnanyaDash(UserTestSteps.java:47)\r\n\tat ✽.And I enter Employee Name Ananya Dash(userstest.feature:13)\r\n",
+  "status": "failed"
+});
+formatter.match({
+  "location": "UserTestSteps.iEnterUserName()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.match({
+  "location": "UserTestSteps.iSelectStatusDisable()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.match({
+  "location": "UserTestSteps.iEnterPassword()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.match({
+  "location": "UserTestSteps.iEnterConfirmPassword()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.match({
+  "location": "UserTestSteps.iClickOnSaveButton()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.match({
+  "location": "UserTestSteps.iVerifyMessageSuccessfullySaved()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.after({
+  "duration": 848821800,
+  "status": "passed"
+});
+});
